2121c5a13dd6d6442e751bfb138fe7d36323f1bc
zainabdnaya <diyanazizo13@gmail.com>
2021-05-01 17:10:40 +0000

    check-error

--- philo_one/mutex_forks_eat.c
+++ philo_one/mutex_forks_eat.c
@@ -6,7 +6,7 @@
 /*   By: zdnaya <zdnaya@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/29 00:24:48 by zainabdnaya       #+#    #+#             */
-/*   Updated: 2021/05/01 17:05:41 by zdnaya           ###   ########.fr       */
+/*   Updated: 2021/05/01 17:09:43 by zdnaya           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,17 @@ void pickup_forks(t_philo_state *philo)
     if (pthread_mutex_lock(&philo->forks[philo->right_fork]) == 0)
         display_msg(philo, 4);
     if (pthread_mutex_lock(&philo->forks[philo->left_fork]) == 0)
-    {
         display_msg(philo, 4);
-        // philo->status = EAT;
-        usleep(100);
-    }
 }
 
 void eating_time(t_philo_state *philo)
 {
-    // if (philo->status == EAT)
-    {
         pthread_mutex_lock(&philo->is_eating);
         display_msg(philo, 2);
         usleep(1000 * (philo->eat));
         philo->last_meal = time_data();
         usleep(100);
         pthread_mutex_unlock(&philo->is_eating);
-    }
 }
 
 void put_down_forks(t_philo_state *philo)
@@ -43,7 +36,6 @@ void put_down_forks(t_philo_state *philo)
     pthread_mutex_unlock(&philo->forks[philo->right_fork]);
     pthread_mutex_unlock(&philo->forks[philo->left_fork]);
     usleep(100);
-
     display_msg(philo, 3);
     usleep(philo->sleep * 1000);
 }
